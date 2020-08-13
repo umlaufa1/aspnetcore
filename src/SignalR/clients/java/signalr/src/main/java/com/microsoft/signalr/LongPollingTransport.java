72add698ac3c1d7ea101ae36bb118c29f78c8819
BrennanConroy <brecon@microsoft.com>
2020-08-13 13:12:42 -0700

    fb

--- src/SignalR/clients/java/signalr/src/main/java/com/microsoft/signalr/LongPollingTransport.java
+++ src/SignalR/clients/java/signalr/src/main/java/com/microsoft/signalr/LongPollingTransport.java
@@ -80,8 +80,7 @@ class LongPollingTransport implements Transport {
                     }, e -> {
                         this.stop().onErrorComplete().subscribe();
                     });
-                    poll(url)
-                    .subscribeWith(receiveLoop);
+                    poll(url).subscribeWith(receiveLoop);
                 });
 
                 return Completable.complete();
@@ -160,10 +159,10 @@ class LongPollingTransport implements Transport {
                 HttpRequest request = new HttpRequest();
                 request.addHeaders(headers);
                 return this.pollingClient.delete(this.url, request).ignoreElement()
-                .andThen(receiveLoop)
-                .doOnComplete(() -> {
-                    cleanup(this.closeError);
-                });
+                    .andThen(receiveLoop)
+                    .doOnComplete(() -> {
+                        cleanup(this.closeError);
+                    });
             })).doOnError(e -> {
                 cleanup(e.getMessage());
             });
